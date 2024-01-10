0bdfb326c4553ceba976c7a7b5a4792d613d67b2
Katy Ekey <katy.ekey@gmail.com>
2024-01-10 15:00:29 -0500

    all standards are being checked for their presence
    [#standards-checker]

--- chpl/chpl-api/src/main/java/gov/healthit/chpl/ValidateStandardApp.java
+++ chpl/chpl-api/src/main/java/gov/healthit/chpl/ValidateStandardApp.java
@@ -22,8 +22,8 @@ import gov.healthit.chpl.standard.Standard;
 
 public class ValidateStandardApp {
 
-    private static String activeCertificateFile = "C://CHPL//chpl-active-20240109_065450.json";
-    private static String inactiveCertificateFile = "C://CHPL//chpl-inactive-20240109_072757.json";
+    private static String activeCertificateFile = "C://CHPL//chpl-active-20240110_164729.json";
+    private static String inactiveCertificateFile = "C://CHPL//chpl-inactive-20240110_171024.json";
 
     private static Map<CertificationCriterion, List<Standard>> standardRequirements;
 
@@ -34,35 +34,43 @@ public class ValidateStandardApp {
         JsonFactory jsonFactory = new JsonFactory(); // or, for data binding, org.codehaus.jackson.mapper.MappingJsonFactory
         jsonFactory.setCodec(objectMapper);
 
-        CertificationCriterion criterion = CertificationCriterion.builder().id(172L).build();
-        Standard standard = Standard.builder()
-                .regulatoryTextCitation("170.205(k)(3)")
-                .endDay(null)
-                .build();
+//        CertificationCriterion criterion = CertificationCriterion.builder().id(58L).build();
+//        Standard standard = Standard.builder()
+//                .regulatoryTextCitation("170.205(a)(4)")
+//                .endDay(null)
+//                .build();
 
         System.out.println("Loading listings from " + activeCertificateFile);
         JsonParser jParser = jsonFactory.createParser(new File(activeCertificateFile)); // or URL, Stream, Reader, String, byte[]
         List<CertifiedProductSearchDetails> listings = objectMapper.readValue(
                 jParser, new TypeReference<List<CertifiedProductSearchDetails>>() { });
-        reviewListingsHaveStandardOnExpectedCriterion(listings, criterion, standard);
-        //TODO: review standard isn't on any other criteria
+
+        for (CertificationCriterion criterion : standardRequirements.keySet()) {
+            for (Standard standard : standardRequirements.get(criterion)) {
+                reviewListingsHaveStandardOnExpectedCriterion(listings, criterion, standard);
+                //TODO: review standard isn't on any other criteria
+            }
+        }
+
 
         System.out.println("Loading listings from " + inactiveCertificateFile);
         jParser = jsonFactory.createParser(new File(inactiveCertificateFile)); // or URL, Stream, Reader, String, byte[]
         listings = objectMapper.readValue(jParser, new TypeReference<List<CertifiedProductSearchDetails>>() { });
-        reviewListingsHaveStandardOnExpectedCriterion(listings, criterion, standard);
-        //TODO: review standard isn't on any other criteria
+
+        for (CertificationCriterion criterion : standardRequirements.keySet()) {
+            for (Standard standard : standardRequirements.get(criterion)) {
+                reviewListingsHaveStandardOnExpectedCriterion(listings, criterion, standard);
+                //TODO: review standard isn't on any other criteria
+            }
+        }
     }
 
     private static void reviewListingsHaveStandardOnExpectedCriterion(
             List<CertifiedProductSearchDetails> listings, CertificationCriterion criterion, Standard standard) {
-        int listingCount = 0;
-        int listingsWithCriterionCount = 0;
-        System.out.println("Iterating through each listing...");
         for (CertifiedProductSearchDetails listing : listings) {
             CertificationResult attestedCriterion = getCertResult(listing, criterion);
             if (attestedCriterion != null) {
-                listingsWithCriterionCount++;
+                //listingsWithCriterionCount++;
                 if (!certResultContainsStandard(attestedCriterion, standard)
                         && listingInactiveDateIsBeforeStandardEndDate(listing.getDecertificationDay(), standard)
                         && listingCertDateIsBeforeStandardEndDate(listing.getCertificationDay(), standard)) {
@@ -70,10 +78,7 @@ public class ValidateStandardApp {
                             + listing.getChplProductNumber());
                 }
             }
-            listingCount++;
         }
-        System.out.println("# listings with criterion " + criterion.getId() + ": " + listingsWithCriterionCount);
-        System.out.println("# listings: " + listingCount);
     }
 
     private static void buildStandardRequirements() {
@@ -81,7 +86,7 @@ public class ValidateStandardApp {
 
         standardRequirements.put(CertificationCriterion.builder().id(16L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.202(a)(2)").build(),
-                        Standard.builder().regulatoryTextCitation("170.202(d))").build(),
+                        Standard.builder().regulatoryTextCitation("170.202(d)").build(),
                         Standard.builder().regulatoryTextCitation("170.205(a)(3)").build(),
                         Standard.builder().regulatoryTextCitation("170.205(a)(4)").build(),
                         Standard.builder().regulatoryTextCitation("170.205(p)(1)").build()).toList());
@@ -117,10 +122,27 @@ public class ValidateStandardApp {
                         Standard.builder().regulatoryTextCitation("170.205(k)(1)").build(),
                         Standard.builder().regulatoryTextCitation("170.205(k)(2)").build()).toList());
 
+        standardRequirements.put(CertificationCriterion.builder().id(30L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(g)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(31L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(g)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(35L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(a)(2)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(36L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(c)(2)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(37L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(a)(2)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(c)(2)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(38L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(g)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(39L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(d)").build()).toList());
+
         standardRequirements.put(CertificationCriterion.builder().id(40L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.204(a)(1)").build(),
                         Standard.builder().regulatoryTextCitation("170.204(a)(2)").build(),
-                        Standard.builder().regulatoryTextCitation("170.205(a)(4)").build()).toList());
+                        Standard.builder().regulatoryTextCitation("170.205(a)(4)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(g)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(43L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.205(e)(4)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(44L).build(),
@@ -134,6 +156,8 @@ public class ValidateStandardApp {
         standardRequirements.put(CertificationCriterion.builder().id(49L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.205(s)(1)").build()).toList());
 
+        standardRequirements.put(CertificationCriterion.builder().id(52L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.315(g)(3)(iv)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(55L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.205(a)(4)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(58L).build(),
@@ -143,19 +167,23 @@ public class ValidateStandardApp {
                         Standard.builder().regulatoryTextCitation("170.202(e)(1)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(60L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.202(a)(2)").build(),
-                        Standard.builder().regulatoryTextCitation("170.202(b))").build(),
-                        Standard.builder().regulatoryTextCitation("170.202(d))").build(),
+                        Standard.builder().regulatoryTextCitation("170.202(b)").build(),
+                        Standard.builder().regulatoryTextCitation("170.202(d)").build(),
                         Standard.builder().regulatoryTextCitation("170.202(e)(1)").build()).toList());
 
         standardRequirements.put(CertificationCriterion.builder().id(165L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.202(a)(2)").build(),
-                        Standard.builder().regulatoryTextCitation("170.202(d))").build(),
+                        Standard.builder().regulatoryTextCitation("170.205(a)(5)").build(),
+                        Standard.builder().regulatoryTextCitation("170.202(d)").build(),
                         Standard.builder().regulatoryTextCitation("170.205(a)(3)").build(),
                         Standard.builder().regulatoryTextCitation("170.205(a)(4)").build(),
-                        Standard.builder().regulatoryTextCitation("170.205(p)(1)").build()).toList());
+                        Standard.builder().regulatoryTextCitation("170.205(p)(1)").build(),
+                        Standard.builder().regulatoryTextCitation("170.213(a)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(166L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.205(a)(3)").build(),
-                        Standard.builder().regulatoryTextCitation("170.205(a)(4)").build()).toList());
+                        Standard.builder().regulatoryTextCitation("170.205(a)(4)").build(),
+                        Standard.builder().regulatoryTextCitation("170.205(a)(5)").build(),
+                        Standard.builder().regulatoryTextCitation("170.213(a)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(167L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.205(b)(1)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(168L).build(),
@@ -166,22 +194,56 @@ public class ValidateStandardApp {
                         Standard.builder().regulatoryTextCitation("170.205(o)(1)").build()).toList());
 
         standardRequirements.put(CertificationCriterion.builder().id(170L).build(),
-                Stream.of(Standard.builder().regulatoryTextCitation("170.205(a)(4)").build()).toList());
+                Stream.of(Standard.builder().regulatoryTextCitation("170.205(a)(4)").build(),
+                        Standard.builder().regulatoryTextCitation("170.205(a)(5)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(172L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.205(h)(2)-Cures").endDay(LocalDate.parse("2022-12-31")).build(),
                         Standard.builder().regulatoryTextCitation("170.205(h)(3)").build(),
                         Standard.builder().regulatoryTextCitation("170.205(k)(1)-Cures").endDay(LocalDate.parse("2022-12-31")).build(),
                         Standard.builder().regulatoryTextCitation("170.205(k)(2)-Cures").endDay(LocalDate.parse("2022-12-31")).build(),
                         Standard.builder().regulatoryTextCitation("170.205(k)(3)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(173L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(e)(1)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(e)(2)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(e)(3)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(h)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(174L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(e)(1)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(e)(2)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(e)(3)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(h)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(175L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(e)(1)").build(),
+                        Standard.builder().regulatoryTextCitation("170.210(h)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(176L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.210(a)(2)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(178L).build(),
                 Stream.of(Standard.builder().regulatoryTextCitation("170.204(a)(1)").build(),
                         Standard.builder().regulatoryTextCitation("170.204(a)(2)").build(),
-                        Standard.builder().regulatoryTextCitation("170.205(a)(4)").build()).toList());
+                        Standard.builder().regulatoryTextCitation("170.205(a)(4)").build(),
+                        Standard.builder().regulatoryTextCitation("170.205(a)(5)").build(),
+                        Standard.builder().regulatoryTextCitation("170.213(a)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(179L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.213(a)").build()).toList());
 
         standardRequirements.put(CertificationCriterion.builder().id(180L).build(),
-                Stream.of(Standard.builder().regulatoryTextCitation("170.205(a)(4)").build()).toList());
+                Stream.of(Standard.builder().regulatoryTextCitation("170.205(a)(4)").build(),
+                        Standard.builder().regulatoryTextCitation("170.205(a)(5)").build(),
+                        Standard.builder().regulatoryTextCitation("170.213(a)").build()).toList());
         standardRequirements.put(CertificationCriterion.builder().id(181L).build(),
-                Stream.of(Standard.builder().regulatoryTextCitation("170.205(a)(4)").build()).toList());
+                Stream.of(Standard.builder().regulatoryTextCitation("170.205(a)(4)").build(),
+                        Standard.builder().regulatoryTextCitation("170.205(a)(5)").build(),
+                        Standard.builder().regulatoryTextCitation("170.213(a)").build()).toList());
+        standardRequirements.put(CertificationCriterion.builder().id(182L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.213(a)").build(),
+                        Standard.builder().regulatoryTextCitation("170.215(a)(1)").build(),
+                        Standard.builder().regulatoryTextCitation("170.215(a)(2)").build(),
+                        Standard.builder().regulatoryTextCitation("170.215(a)(3)").build(),
+                        Standard.builder().regulatoryTextCitation("170.215(a)(4)").build(),
+                        Standard.builder().regulatoryTextCitation("170.215(b)").build()).toList());
+
+        standardRequirements.put(CertificationCriterion.builder().id(210L).build(),
+                Stream.of(Standard.builder().regulatoryTextCitation("170.213(a)").build()).toList());
     }
 
     private static CertificationResult getCertResult(CertifiedProductSearchDetails listing, CertificationCriterion criterion) {
