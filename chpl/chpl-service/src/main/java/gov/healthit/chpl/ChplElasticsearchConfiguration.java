eb44d5fcc10fe5332a2a89d4821d0c45dd40136c
Katy Ekey <katy.ekey@gmail.com>
2024-02-10 13:29:34 -0500

    feat: Add config for Elasticsearch
    [#elastic]

---
+++ chpl/chpl-service/src/main/java/gov/healthit/chpl/ChplElasticsearchConfiguration.java
@@ -0,0 +1,16 @@
+package gov.healthit.chpl;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ChplElasticsearchConfiguration extends ElasticsearchConfiguration  {
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+            .connectedTo("localhost:9200")
+            .build();
+    }
+}
